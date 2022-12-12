f753da4364e1cb964109df885484e9f93c51f040
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-12 22:52:16 +0600

    Add db context

---
+++ AkvelonTestTask/Models/Context/ProjectTasksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AkvelonTestTask.Models.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace AkvelonTestTask.Models.Context
+{
+    public class ProjectTasksDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        public ProjectTasksDbContext(DbContextOptions<ProjectTasksDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
