6b8d63483a790518e983e85effcfd0111bde3da1
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 00:38:05 +0600

    Add model to DTO

---
+++ AkvelonTestTask/Models/DTO/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AkvelonTestTask.Enums;
+
+namespace AkvelonTestTask.Models.DTO
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public ProjectStatuses Status { get; set; }
+        public int Priority { get; set; }
+    }
+}
