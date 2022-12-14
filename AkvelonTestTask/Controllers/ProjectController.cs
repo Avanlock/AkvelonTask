ff2e132e88e0e3c0c6be07a7733173fbf70e0788
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 00:38:36 +0600

    Add ProjectController

--- AkvelonTestTask/Controllers/ProjectController.cs
+++ AkvelonTestTask/Controllers/ProjectController.cs
@@ -1,9 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AkvelonTestTask.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AkvelonTestTask.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class ProjectController : Controller
     {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IMapper _mapper;
+
+        public ProjectController(IProjectRepository projectRepository, IMapper mapper)
+        {
+            _projectRepository = projectRepository;
+            _mapper = mapper;
+        }
         
+        [HttpGet]
+        public async Task<ActionResult<List<Models.DTO.Project>>> GetAllProjects()
+        {
+            var projects = await _projectRepository.GetAllProjectsAsync();
+            var projectsDTO = _mapper.Map<List<Models.DTO.Project>>(projects);
+            return Ok(projectsDTO);
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Models.DTO.Project>>> GetProjectById(Guid id)
+        {
+            var projects = await _projectRepository.GetProjectByIdAsync(id);
+            var projectsDTO = _mapper.Map<List<Models.DTO.Project>>(projects);
+            return Ok(projectsDTO);
+        }
     }
 }
