6df32ae6a4f25518113f1d2c543e0facf0b53a22
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 00:39:21 +0600

    Add Repository

---
+++ AkvelonTestTask/Repositories/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AkvelonTestTask.Models.Context;
+using AkvelonTestTask.Models.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace AkvelonTestTask.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly ProjectTasksDbContext _context;
+
+        public ProjectRepository(ProjectTasksDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Project>> GetAllProjectsAsync() => await _context.Projects.ToListAsync();
+
+        public async Task<Project> GetProjectByIdAsync(Guid id) =>
+            await _context.Projects.FirstOrDefaultAsync(p => p.Id == id);
+    }
+}
