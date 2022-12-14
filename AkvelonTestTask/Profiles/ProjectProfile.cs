62227178517c6a5a9da36f14a0b45c33b0fe524f
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 00:39:06 +0600

    Add AutoMapper ProjectProfile

---
+++ AkvelonTestTask/Profiles/ProjectProfile.cs
@@ -0,0 +1,14 @@
+﻿using AkvelonTestTask.Models.DbModels;
+using AutoMapper;
+
+namespace AkvelonTestTask.Profiles
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<Project, Models.DTO.Project>().ReverseMap();
+        }
+
+    }
+}
