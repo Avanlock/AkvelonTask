4711b95dd00e4ad2bc4fe7081c5a1ecc4289ecde
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 00:37:27 +0600

    Edit Project model

--- AkvelonTestTask/Models/DbModels/Project.cs
+++ AkvelonTestTask/Models/DbModels/Project.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using AkvelonTestTask.Models.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using AkvelonTestTask.Enums;
 
 namespace AkvelonTestTask.Models.DbModels
 {
     public class Project
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime CompletionDate { get; set; }
         public ProjectStatuses Status { get; set; }
         public int Priority { get; set; }
+        public IEnumerable<Task> Tasks { get; set; }
     }
 }
