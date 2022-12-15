8911dbbf1e59ec7b620cbdc6c4adb65d200d425f
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:42:30 +0600

    small fixes

--- AkvelonTestTask/Models/DbModels/Task.cs
+++ AkvelonTestTask/Models/DbModels/Task.cs
@@ -7,13 +7,12 @@ namespace AkvelonTestTask.Models.DbModels
 {
     public class Task
     {
-        [Key]
-        [JsonIgnore]
         public Guid Id { get; set; }
         public string Name { get; set; }
         public TaskStatuses Status { get; set; }
         public Guid ProjectId { get; set; }
         public string Description { get; set; }
+        [JsonIgnore]
         public Project Project { get; set; }
     }
 }
