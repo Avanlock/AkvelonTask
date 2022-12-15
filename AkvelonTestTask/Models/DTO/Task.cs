e53d6296973d53d560e4d454e2de3e707c7e61ec
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:42:47 +0600

    Add DTO task model

---
+++ AkvelonTestTask/Models/DTO/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using AkvelonTestTask.Enums;
+
+namespace AkvelonTestTask.Models.DTO
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public TaskStatuses Status { get; set; }
+        public Guid ProjectId { get; set; }
+        public string Description { get; set; }
+        [JsonIgnore]
+        public Project Project { get; set; }
+    }
+}
