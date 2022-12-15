4a88b21d9cd870e110373da165e1579ae6976c73
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:42:18 +0600

    small fixes

--- AkvelonTestTask/Startup.cs
+++ AkvelonTestTask/Startup.cs
@@ -23,14 +23,14 @@ namespace AkvelonTestTask
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            string connection = Configuration.GetConnectionString("DbConnectionString");
+            var connection = Configuration.GetConnectionString("DbConnectionString");
             services.AddDbContext<ProjectTasksDbContext>(option => option.UseNpgsql(connection));
             services.AddScoped<IProjectRepository, ProjectRepository>();
             services.AddScoped<ITaskRepository, TaskRepository>();
