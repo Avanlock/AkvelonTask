b4644363fbbdc2d0d5bc59e6b3cac9acafb47f26
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:40:45 +0600

    Add request models

---
+++ AkvelonTestTask/Models/DTO/AddProjectRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AkvelonTestTask.Enums;
+
+namespace AkvelonTestTask.Models.DTO
+{
+    public class AddProjectRequest
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public ProjectStatuses Status { get; set; }
+        public int Priority { get; set; }
+    }
+}
---
+++ AkvelonTestTask/Models/DTO/AddTaskRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using AkvelonTestTask.Enums;
+
+namespace AkvelonTestTask.Models.DTO
+{
+    public class AddTaskRequest
+    {
+        public string Name { get; set; }
+        public TaskStatuses Status { get; set; }
+        public Guid ProjectId { get; set; }
+        public string Description { get; set; }
+        [JsonIgnore]
+        public Project Project { get; set; }
+    }
+}
