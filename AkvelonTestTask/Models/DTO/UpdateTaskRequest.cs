6125cd1221be0436e01beb237ef35deac7ea8a21
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:44:29 +0600

    Add Update Requests model

---
+++ AkvelonTestTask/Models/DTO/UpdateTaskRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using AkvelonTestTask.Enums;
+
+namespace AkvelonTestTask.Models.DTO
+{
+    public class UpdateTaskRequest
+    {
+        public string Name { get; set; }
+        public TaskStatuses Status { get; set; }
+        public string Description { get; set; }
+    }
+}
