aec4bbc0c7021ff8b3e2a15019435b1ec59e317d
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:43:15 +0600

    Add TaskProfile to automapper

---
+++ AkvelonTestTask/Profiles/TaskProfile.cs
@@ -0,0 +1,13 @@
+﻿using AkvelonTestTask.Models.DbModels;
+using AutoMapper;
+
+namespace AkvelonTestTask.Profiles
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<Task, Models.DTO.Task>().ReverseMap();
+        }
+    }
+}
