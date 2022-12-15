38bad973ab4bb3efc5d873217f06fdd55e4a002d
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-12-15 23:41:56 +0600

    add methods to ProjectRepository

--- AkvelonTestTask/Repositories/ProjectRepository.cs
+++ AkvelonTestTask/Repositories/ProjectRepository.cs
@@ -17,9 +17,52 @@ namespace AkvelonTestTask.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Project>> GetAllProjectsAsync() => await _context.Projects.ToListAsync();
+        public async Task<IEnumerable<Project>> GetAllProjectsAsync()
+        {
+            var projects = await _context.Projects.ToListAsync();
+            if (projects.Count == 0)
+                return null;
+            return projects;
+        }
 
-        public async Task<Project> GetProjectByIdAsync(Guid id) =>
-            await _context.Projects.FirstOrDefaultAsync(p => p.Id == id);
+        public async Task<Project> GetProjectByIdAsync(Guid id)
+        {
+            var project = await _context.Projects.FirstOrDefaultAsync(p => p.Id == id);
+            if (project is null)
+                return null;
+            return project;
+        }
+
+        public async Task<Project> AddProjectAsync(Project project)
+        {
+            project.Id = Guid.NewGuid();
+            await _context.Projects.AddAsync(project);
+            await _context.SaveChangesAsync();
+            return project;
+        }
+
+        public async Task<Project> DeleteProjectAsync(Guid id)
+        {
+            var project = await _context.Projects.FirstOrDefaultAsync(p => p.Id == id);
+            if (project is null)
+                return null;
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+            return project;
+        }
+
+        public async Task<Project> UpdateProjectAsync(Guid id, Project project)
+        {
+            var exisingProject = await _context.Projects.FirstOrDefaultAsync(p => p.Id == id);
+            if (exisingProject is null)
+                return null;
+            exisingProject.Name = project.Name;
+            exisingProject.Priority = project.Priority;
+            exisingProject.Status = project.Status;
+            exisingProject.CompletionDate = project.CompletionDate;
+            exisingProject.StartDate = project.StartDate;
+            await _context.SaveChangesAsync();
+            return exisingProject;
+        }
     }
 }
